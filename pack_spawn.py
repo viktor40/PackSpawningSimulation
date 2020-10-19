51d0ddf2a387e52ed83b9fa425ee50d1b8052f0e
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-10-19 18:36:05 +0200

    kinda works but isn't symmetrical

--- pack_spawn.py
+++ pack_spawn.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 chances_x = {}
 chances_xz = {}
 
-pack_size = 4
+pack_size = 5
 iterations = 0
 
 for j in range(6):
     for k in range(6):
-        offset_abs = np.abs(j - k)
+        offset_abs = j - k
         if offset_abs not in chances_x:
             chances_x[offset_abs] = 1
         else:
@@ -24,7 +24,7 @@ for i in range(pack_size - 1):
         for l in range(k):
             for m in range(6):
                 for n in range(6):
-                    offset_abs = j + np.abs(m - n)
+                    offset_abs = j + (m - n)
                     if offset_abs not in chances_x:
                         chances_x[offset_abs] = 1
                     else:
@@ -34,7 +34,6 @@ for i in range(pack_size - 1):
 
 
 sorted_items = sorted(chances_x.items())
-print(sorted_items)
 
 sorted_x = []
 
@@ -52,13 +51,15 @@ for i, j in sorted_x:
             chances_xz[square_radius] += j * l
 
 sorted_items_xz = sorted(chances_xz.items())
+print(sorted_items_xz)
 cumulative_chances, x, z = [], [], []
 cum = 0
+
 for i, j in sorted_items_xz:
     cum += j
+    cumulative_chances.append((i, round(cum, 10) * 100))
     x.append(i)
     z.append(round(cum, 10) * 100)
-    cumulative_chances.append((i, round(cum, 10) * 100))
 
 print(cumulative_chances)
 plt.plot(x, z)
